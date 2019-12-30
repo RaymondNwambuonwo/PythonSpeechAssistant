da432e580dea0922dbbe924c29551f6ab1e81269
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-12-29 23:21:51 -0500

    try block for unrecognizable speech

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say saomething")  # Prompt User
     audio = r.listen(source)  # Audio variable with source
-    voice_data = r.recognize_google(audio)  # variable using recognize google
-    print(voice_data)  # shows what user said
+   # try block and this allows for unrecognizable speech and throws an error
+    try:
+        # variable using recognize google
+        voice_data = r.recognize_google(audio)
+        print(voice_data)  # shows what user said
+    except sr.UnknownValueError:
+        print("Sorry, I did not catch that")
+    except sr.RequestError:
+        print("My apologies, my speech is on the fritz")
