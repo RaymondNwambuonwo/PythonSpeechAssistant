8fa8cdca396c0a6bb8633f0949881539b95eb008
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-12-29 22:51:28 -0500

    microphone test setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+# initialize recognizer class which is responsible for recognizing speech
+r = sr.Recognizer()
+
+# Using microphone as source of audio input
+with sr.Microphone() as source:
+    print("Say saomething")  # Prompt User
+    audio = r.listen(source)  # Audio variable with source
+    voice_data = r.recognize_google(audio)  # variable using recognize google
+    print(voice_data)
