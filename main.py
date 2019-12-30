9bbc4c69e3c944a76a53736422e32638447baf0a
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-12-30 00:40:31 -0500

    added function for assistant voice

--- main.py
+++ main.py
@@ -29,6 +29,16 @@ def record_audio(ask=False):
         return voice_data
 
 
+def claudia_speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en')
+    rd = random.randint(1, 10000000)
+    audio_file = 'audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
+
+
 def respond(voice_data):  # function for response from Claudia
     if 'what is your name' in voice_data:
         print('My name is Claudia')
